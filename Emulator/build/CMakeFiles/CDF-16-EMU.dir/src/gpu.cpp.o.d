995cf6289551170d978360a25030301f35810391
Charles Fletcher <fletcherdcharles@gmail.com>
2024-01-02 10:53:30 +0000

    Fixed Logging System

--- Emulator/build/CMakeFiles/CDF-16-EMU.dir/src/gpu.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/CDF-16-EMU.dir/src/gpu.cpp.o: \
- /home/dcsrock1/Projects/CDF-16/Emulator/src/gpu.cpp \
- /usr/include/stdc-predef.h
--- Emulator/build/CMakeFiles/CDF-16-EMU.dir/src/memory.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/CDF-16-EMU.dir/src/memory.cpp.o: \
- /home/dcsrock1/Projects/CDF-16/Emulator/src/memory.cpp \
- /usr/include/stdc-predef.h
