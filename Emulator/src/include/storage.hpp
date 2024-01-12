188a40572f322f3dc42ea9562e18b4e48b951867
Charles fletcher <fletcherdcharles@gmail.com>
2024-01-12 21:39:38 +0000

    Storage System added

--- Emulator/src/include/storage.hpp
+++ Emulator/src/include/storage.hpp
@@ -3,8 +3,10 @@
 #include <array>
 class Storage {
 public:
-    uint8_t getByte(uint16_t address, bool bankSelector, uint64_t& cycles);
-    uint16_t getWord(uint16_t address, bool bankSelector, uint64_t& cycles);
+    int8_t getByte(uint16_t address, bool bankSelector, uint64_t& cycles);
+    int16_t getWord(uint16_t address, bool bankSelector, uint64_t& cycles);
+    void setByte(uint16_t address, int8_t data, bool bankSelector, uint64_t& cycles);
+    void setWord(uint16_t address, int16_t data, bool bankSelector, uint64_t& cycles);
     void reset();
 private:
     std::array<uint8_t, 65536> storageB0, storageB1;
--- Emulator/src/storage.cpp
+++ Emulator/src/storage.cpp
@@ -1,5 +1,29 @@
 #include "include/storage.hpp"
 
+
+int8_t Storage::getByte(uint16_t address, bool bankSelector, uint64_t& cycles) {
+    cycles++;
+    if (bankSelector) {
+        return storageB1.at(address);
+    } else {
+        return storageB0.at(address);
+    }
+
+}
+int16_t Storage::getWord(uint16_t address, bool bankSelector, uint64_t& cycles) {
+    cycles += 2;
+    if (bankSelector) {
+        int16_t mainByte = storageB1.at(address + 1);
+        int16_t secondaryByte = storageB1.at(address);
+        int16_t result = ((mainByte << 8) | secondaryByte); 
+        return result;
+    } else {
+        int16_t mainByte = storageB0.at(address + 1);
+        int16_t secondaryByte = storageB0.at(address);
+        int16_t result = ((mainByte << 8) | secondaryByte);
+        return result;
+    }
+}
 void Storage::reset() {
     
 }
