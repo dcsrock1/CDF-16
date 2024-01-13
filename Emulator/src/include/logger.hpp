05a6bc1474054726420cff56b988d58f96b3c159
Charles fletcher <fletcherdcharles@gmail.com>
2024-01-13 20:33:09 +0000

    Comment additions

--- Emulator/src/include/logger.hpp
+++ Emulator/src/include/logger.hpp
@@ -3,7 +3,6 @@
 
 #define LOG_FILE "emulator.log" //Compile time definition for the log file (likely to change)
 
-
 void logDebug(std::string msg); //Debug logging function, prints the message to the console in blue.
 
 void logInfo(std::string msg); //Info logging function, prints the message to the console in green.
